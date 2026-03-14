badbc12aa8fa4be34efe751ec03d915311d7264f
Avatar-Jisan <abdullaaljisan@gmail.com>
2026-03-14 07:51:41 +0600

    Ajex on game.html
    layout has been improved . game.html now shows data from json .

--- js/game.js
+++ js/game.js
@@ -1,3 +1,15 @@
 const parameter = new URLSearchParams(window.location.search);
 const gameId = parameter.get("id");
-console.log(gameId);
+
+$.getJSON("data/games.json", function(games) {
+    const game= games.find(g => g.id === gameId);
+    $("#gameTitle1").text(game.name);
+    $("#gameTitle").text(game.name);
+    $("#gameDeveloper").text(game.developer);
+    $("#gameCategory").text(game.category.join(", "));
+    $("#gameDescription").text(game.longDescription);
+    
+    game.rules.forEach(element => {
+        $("#gameRules").append(`<li>${element}</li>`);
+    });
+});
