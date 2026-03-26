c123738284b1989643f909bfbc1c78d3adab3484
Avatar-Jisan <abdullaaljisan@gmail.com>
2026-03-26 19:48:01 +0600

    lol

--- games/Ludo/ludo.js
+++ games/Ludo/ludo.js
@@ -46,23 +46,33 @@ function getCell(row, col){
     return board.children[row * 15 + col];
 }
 // color bases
-for(let r=0; r<6; r++){
-    for(let c=0; c<6; c++){
-        getCell(r,c).classList.add("base-red");
-        getCell(14-r, c).classList.add("base-blue");
-        getCell(r, 14-c).classList.add("base-green");
-        getCell(14-r, 14-c).classList.add("base-yellow");
+function fillBase(baseId, color) {
+    const base = document.getElementById(baseId);
+
+    const inner = document.createElement("div");
+    inner.classList.add("base-inner");
+
+    for (let i = 0; i < 4; i++) {
+        const slot = document.createElement("div");
+        slot.classList.add("token-slot");
+
+        const token = document.createElement("div");
+        token.classList.add("token", color);
+
+        slot.appendChild(token);
+        inner.appendChild(slot);
     }
+
+    base.appendChild(inner);
 }
-// inner white squares
-for(let r=1;r<=4;r++){
-    for(let c=1;c<=4;c++){
-        getCell(r,c).classList.add("white");
-        getCell(14-r, c).classList.add("white");
-        getCell(r, 14-c).classList.add("white");
-        getCell(14-r, 14-c).classList.add("white");
-    }
-}
+
+// INIT
+fillBase("redBase", "red");
+fillBase("greenBase", "green");
+fillBase("yellowBase", "yellow");
+fillBase("blueBase", "blue");
+
+
 // Cross paths
 for(let i=0;i<15;i++){
     for(let j=6;j<9;j++){
@@ -117,6 +127,8 @@ function createBaseTokens(color, startRow, startCol) {
     const container = document.createElement("div");
     container.classList.add("base-inner");
 
+    container.style.gridRow = `${startRow+1}/span 4`;
+    container.style.gridColumn=`${startCol+1}/span 4`;
     // Create 4 slots
     for (let i = 0; i < 4; i++) {
         const slot = document.createElement("div");
@@ -137,20 +149,20 @@ function hideWhiteCells(startRow, startCol){
     for(let r=startRow;r<startRow+4;r++){
         for(let c=startCol; c<startCol+4;c++){
             if(r===startRow+1 && c===startCol+1) continue;
-            getCell(r,c).style.display="none";
+            getCell(r,c).style.visibility="none";
         }
     }
 }
 
 // RED (top-left)
 createBaseTokens("red", 0, 0);
-
+hideWhiteCells(0,0);
 // GREEN (top-right)
 createBaseTokens("green", 0, 9);
-
+hideWhiteCells(0,9);
 // YELLOW (bottom-left)
 createBaseTokens("yellow", 9, 0);
-
+hideWhiteCells(9,0);
 // BLUE (bottom-right)
 createBaseTokens("blue", 9, 9);
-
+hideWhiteCells(9,9);
