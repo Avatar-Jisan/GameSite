98bb6f9c87ebac5da70354e6c0b97ef399464416
Avatar-Jisan <abdullaaljisan@gmail.com>
2026-04-11 03:39:41 +0600

    fixed

---
+++ js/profile.js
@@ -0,0 +1,12 @@
+const menuBtn = document.getElementById("menuBtn");
+const sidebar = document.querySelector(".sidebar");
+const overlay = document.getElementById("overlay");
+menuBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+  overlay.classList.toggle("active");
+});
+
+overlay.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+  overlay.classList.remove("active");
+});
